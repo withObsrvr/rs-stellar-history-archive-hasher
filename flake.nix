96d78835e12c6fbe62888c4149ae815893662b13
Tillman Mosley III <tillman@withobsrvr.com>
2026-01-23 07:44:02 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Stellar History Archive Hasher";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            cargo-edit
+            wasm-pack
+            nodejs
+            pkg-config
+            openssl
+          ];
+
+          shellHook = ''
+            echo "Stellar History Archive Hasher development environment"
+            echo "Rust version: $(rustc --version)"
+            echo "Cargo version: $(cargo --version)"
+            echo "wasm-pack available for WebAssembly builds"
+          '';
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "stellar-history-archive-hasher";
+          version = "0.7.0";
+          src = ./.;
+          
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            openssl
+          ];
+        };
+      });
+}
